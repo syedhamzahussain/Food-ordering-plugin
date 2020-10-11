436aae8385bee217df771e373f31dcf64802cf08
syedhamzahussain <40966603+syedhamzahussain@users.noreply.github.com>
2020-10-12 02:15:32 +0500

    version update

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ jQuery( document ).ready(
 						type: "post",
 						data: {action: "wfop_add_to_cart", product_id: id, quantity: quantity,date: date,time: time},
 						success: function (response) {
-							window.location.href;
+							window.location;
 						}
 								}
 				);
