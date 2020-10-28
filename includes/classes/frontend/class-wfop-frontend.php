dfcd0eb8aea3b0396246872b57c4d54e53f4c7e0
syedhamzahussain <40966603+syedhamzahussain@users.noreply.github.com>
2020-10-28 17:46:07 +0500

    improvment in last commit

--- includes/classes/frontend/class-wfop-frontend.php
+++ includes/classes/frontend/class-wfop-frontend.php
@@ -80,14 +80,16 @@ if ( ! class_exists( 'WFOP_FRONTEND' ) ) {
 
 
 		public function filter_function_name( $title ) {
-			 $product_id = $_GET['product_id'];
-			if ($product_id) {
-				
+			
+			if ( isset( $_GET['product_id'] ) ) {
+			
+			$product_id = $_GET['product_id'];
 			$product    = wc_get_product( $product_id );
 
 			if ( $title == 'Single Food Product' && ! empty( $product_id ) ) {
 				return $title = __( $product->get_name() );
-			} 
+			}
+			 
 			}
 
 			return $title;
